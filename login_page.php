ef1d058a1f2afed2746e6b32a474219666f6d803
kshitij314 <kshitijdahal314@gmail.com>
2018-08-05 10:52:00 +0545

    new login page (no dropdown)

---
+++ login_page.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+	<head>
+		<link type = "text/css" rel = "stylesheet" href = "login_page_style.css">
+        <meta charset = "UTF-8">
+	</head>
+	<body>
+		<div id = "login_area">
+			<form method = "POST">
+				Username:
+				<input type = "text" name = "username">
+				<br>
+				Password:
+				<input type = "Password" name = "password">
+				<h1><input type ="button" value = "Login" name = "Submit"></h1>
+			</form>
+		</div>
+	</body>
+</html>
--- not_logged_in_main_page.php
+++ not_logged_in_main_page.php
@@ -8,7 +8,7 @@
     <body>
         <div class = "top_area">
             <div class = "nav_bar">
-                <a href = ""><h1 class = "nav_text">Login</h1></a>
+                <a href = "login_page.php"><button><h1 class = "nav_text">Login</h1></button></a>
             </div>
             <div class = "img_slogan">
               <!-- <h2>Soar</h2> -->
@@ -16,12 +16,24 @@
         </div>
             <div class = "bottom_area">
                 <div id = "img_text">
-                    <div id = "left_img">
-                <img id = "img_src" src = "img/two_people_car.jpg" >
-                    </div>
-                    <div id = "right_text">
-                fasdfsa
-                    </div>
+                    <h3>
+                        ABOUT US
+                    </h3>
+                 <p>
+                    SoarNow is a startup company building an easy way for university students to rideshare they way back home from university or vice versa. Founded in June of 2018 we strive to provide the easiest, cheapest and most convenient way to travel during a students busy postsecondary career.
+
+                    As university students ourselves we have had to face the struggles of using the tedious go transit system with unreliable bus schedules and poor atmosphere of the trip in general.
+
+                    This problem needed a solution ASAP.
+
+                    And that's where SoarNow comes in.
+
+                    We provide students with the means to travel with ease and without the extra tension or effort the the Go Transit system causes. 
+
+                    On top of that we give the option of individuals to make some money on the side by joining our large team of drivers helping us achieve our goals. You make your rides. You pick your price per passenger. And you make the money. 
+
+                    Join SoarNow as a driver or customer today and make your life easier! 
+                    </p>
                 </div>
                 <div id = "become_driver">
                     <input id = "button" type = "button" name = "become_driver_accepted" value = "Become a Driver">
