b8b9a90fdbfa614bcd2b0279d1a1175a4c85c48b
kshitij314 <kshitijdahal314@gmail.com>
2018-08-05 08:31:11 +0545

    first image size increased

--- not_logged_in_main_page.php
+++ not_logged_in_main_page.php
@@ -6,25 +6,29 @@
         <meta charset = "UTF-8">
     </head>
     <body>
-        <div class = "nav_bar">
-            <a href = ""><h1 class = "nav_text">Login</h1></a>
-        </div>
-        <div class = "img_slogan">
-            <img src = "img/car_fog_trees.jpg" height = 200px>
-        </div>
-        <div id = "img_text">
-            <div id = "left_img">
-        <img id = "img_src" src = "img/two_people_car.jpg" >
+        <div class = "top_area">
+            <div class = "nav_bar">
+                <a href = ""><h1 class = "nav_text">Login</h1></a>
             </div>
-            <div id = "right_text">
-        fasdfsa
-            </div>
-        </div>
-       
-        <div id = "become_driver">
+            <div id = "img_slogan">
             
-            <input id = "button" type = "button" name = "become_driver_accepted" value = "Become a Driver">
+            </div>
         </div>
+            <div class = "bottom_area">
+                <div id = "img_text">
+                    <div id = "left_img">
+                <img id = "img_src" src = "img/two_people_car.jpg" >
+                    </div>
+                    <div id = "right_text">
+                fasdfsa
+                    </div>
+                </div>
+
+                <div id = "become_driver">
+
+                    <input id = "button" type = "button" name = "become_driver_accepted" value = "Become a Driver">
+                </div>
+            </div>
         <footer>
         
         
